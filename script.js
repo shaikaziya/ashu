67df60d12f7104f4f92cfad47d536e93a6d1ea83
shaikaziya <shaikaziya1234@gmail.com>
2022-07-30 20:36:16 +0530

    ashu

---
+++ script.js
@@ -0,0 +1,42 @@
+const countdown= document.querySelector(".countdown");
+let count=10;
+countdown.innerText=count--;
+setTimeout(()=>{
+    countdown.innerText=count--;
+    setTimeout(()=>{
+        countdown.innerText=count--;
+        setTimeout(()=>{
+            countdown.innerText=count--;
+            setTimeout(()=>{
+                countdown.innerText=count--;
+                setTimeout(()=>{
+                    countdown.innerText=count--;
+                    setTimeout(()=>{
+                        countdown.innerText=count--;
+                        setTimeout(()=>{
+                            countdown.innerText=count--;
+                            setTimeout(()=>{
+                                countdown.innerText=count--;
+                                setTimeout(()=>{
+                                    countdown.innerText=count--;
+                                    setTimeout(()=>{
+                                        countdown.innerText="जन्मदिन मुबारक"
+                                        setTimeout(()=>{
+                                            countdown.innerText="పుట్టినరోజు శుభాకాంక్షలు";
+                                            setTimeout(()=>{
+                                                countdown.innerText="పుట్టినరోజు శుభాకాంక్షలు";
+                                                setTimeout(()=>{
+                                                    countdown.innerText="Happy Birthday Ashu";
+                                                },1000);
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000)
